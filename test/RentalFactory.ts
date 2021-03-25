c475af69600e8c55b1c6c3a64ab03e9a0c8706aa
Raphael Roullet <raroullet@gmail.com>
2021-03-25 16:11:01 +0100

    feat: test deploying multiple rentals

--- test/RentalFactory.ts
+++ test/RentalFactory.ts
@@ -6,6 +6,7 @@ import { Contract } from "ethers";
 
 import { RentalFactory } from "../typechain";
 import { parseUnits18 } from "../utils/parseUnits";
+import deployMockERC20 from "./utils/mockERC20";
 
 const { deployContract } = hre.waffle;
 const { ethers } = hre;
@@ -14,6 +15,7 @@ describe("RentalFactory", () => {
   let admin: SignerWithAddress;
   let landlord: SignerWithAddress;
   let tenant1: SignerWithAddress;
+  let tenant2: SignerWithAddress;
   let rentalFactory: RentalFactory;
   let dai: Contract;
 
@@ -22,12 +24,13 @@ describe("RentalFactory", () => {
   const rentGuarantee = parseUnits18("1500");
 
   before(async function () {
-    [admin, landlord, tenant1] = await ethers.getSigners();
+    [admin, landlord, tenant1, tenant2] = await ethers.getSigners();
 
-    // deploy an ERC20 token to mock dai
-    const daiFactory = await ethers.getContractFactory("ERC20PresetMinterPauser");
-    dai = await daiFactory.deploy("dai", "DAI");
-    console.log("dai deployed at:", dai.address);
+    console.log(`landlord.address`, landlord.address);
+    console.log(`tenant1.address`, tenant1.address);
+    console.log(`tenant2.address`, tenant2.address);
+
+    dai = await deployMockERC20({ name: "dai", symbol: "DAI" });
 
     const rentalFactoryArtifact: Artifact = await hre.artifacts.readArtifact("RentalFactory");
     rentalFactory = <RentalFactory>await deployContract(admin, rentalFactoryArtifact);
@@ -52,8 +55,42 @@ describe("RentalFactory", () => {
   });
 
   it("should emit an event", async () => {
+    // Deploy a second rental
     await expect(
       rentalFactory.connect(landlord).createNewRental(tenant1.address, rent, deposit, rentGuarantee, dai.address),
     ).to.emit(rentalFactory, "NewRentalDeployed");
+  });
+
+  it("should allow the creation of multiple rentals", async () => {
+    // Given
+    const rent3 = parseUnits18("1000");
+    const deposit3 = parseUnits18("2000");
+    const rentGuarantee3 = parseUnits18("3000");
+
+    // First rental
+    const firstRentalAddress = await rentalFactory.rentalsByOwner(landlord.address, 0);
+    const firstRentalAgreement = await ethers.getContractAt("RentalAgreement", firstRentalAddress);
+
+    expect(await firstRentalAgreement.landlord()).to.eq(landlord.address);
+    expect(await firstRentalAgreement.tenant()).to.eq(tenant1.address);
+
+    // When
+    const createRentalTx = await rentalFactory
+      .connect(landlord)
+      .createNewRental(tenant2.address, rent3, deposit3, rentGuarantee3, dai.address);
+    await createRentalTx.wait();
+
+    // Get this third rental
+    const thirdRentalAddress = await rentalFactory.rentalsByOwner(landlord.address, 2);
+    const thirdRentalAgreement = await ethers.getContractAt("RentalAgreement", thirdRentalAddress);
+
+    // Expect
+    expect(await thirdRentalAgreement.address).to.eq(thirdRentalAddress);
+    expect(await thirdRentalAgreement.landlord()).to.eq(landlord.address);
+    expect(await thirdRentalAgreement.tenant()).to.eq(tenant2.address);
+
+    expect(await thirdRentalAgreement.rent()).to.eq(rent3);
+    expect(await thirdRentalAgreement.deposit()).to.eq(deposit3);
+    expect(await thirdRentalAgreement.rentGuarantee()).to.eq(rentGuarantee3);
   });
 });
